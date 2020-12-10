13be5695f928b22b65e88571834315c7c40aeecb
Dominic Griffin <dom.griffin6@gmail.com>
2020-12-10 14:10:51 +0000

    returning trip lists - tests passing

--- src/components/__tests__/trip-list.js
+++ src/components/__tests__/trip-list.js
@@ -1,19 +1,48 @@
 import getTripList from "../trip-list";
 
 describe("TripList", () => {
+    test("1 geese 0 corn", () => {
+        const cornQuantity = 0;
+        const geeseQuantity = 1;
+
+        const output = [
+            "goose across"
+        ];
+        expect(getTripList(cornQuantity, geeseQuantity)).toEqual(output);
+    });
+    test("0 geese 1 corn", () => {
+        const cornQuantity = 1;
+        const geeseQuantity = 0;
+
+        const output = [
+            "corn across"
+        ];
+        expect(getTripList(cornQuantity, geeseQuantity)).toEqual(output);
+    });
+    test("1 geese 1 corn", () => {
+        const cornQuantity = 1;
+        const geeseQuantity = 1;
+
+        const output = [
+            "corn across",
+            "farmer back",
+            "goose across",
+        ];
+        expect(getTripList(cornQuantity, geeseQuantity)).toEqual(output);
+    });
     test("2 geese 1 corn", () => {
         const cornQuantity = 1;
         const geeseQuantity = 2;
 
-        const output = {
-            1:"corn across",
-            2:"farmer back",
-            3:"goose across",
-            4:"farmer and corn back",
-            5:"goose across",
-            6:"farmer back",
-            7:"corn across"
-        };
+        const output = [
+            "corn across",
+            "farmer back",
+            "goose across",
+            "farmer and corn back",
+            "goose across",
+            "farmer back",
+            "corn across"
+        ];
 
         expect(getTripList(cornQuantity, geeseQuantity)).toEqual(output);
     });
@@ -22,15 +51,15 @@ describe("TripList", () => {
         const cornQuantity = 2;
         const geeseQuantity = 1;
 
-        const output = {
-            1:"goose across",
-            2:"farmer back",
-            3:"corn across",
-            4:"farmer and goose back",
-            5:"corn across",
-            6:"farmer back",
-            7:"goose across"
-        };
+        const output = [
+            "goose across",
+            "farmer back",
+            "corn across",
+            "farmer and goose back",
+            "corn across",
+            "farmer back",
+            "goose across"
+        ];
 
         expect(getTripList(cornQuantity, geeseQuantity)).toEqual(output);
     });
